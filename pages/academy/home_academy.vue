83dac170e296d5b59b4f9b12a5d83b31cf714e2e
MadridM20 <juanmadridm20@gmail.com>
2024-05-07 10:14:37 -0500

    updated academy admin wireframes

---
+++ pages/academy/home_academy.vue
@@ -0,0 +1,30 @@
+<template>
+    <NuxtLayout>
+        <template #firstSpace>
+            <NuxtLink class="boxHome" to="/"><button>Log Out</button></NuxtLink>
+        </template>
+        <div style="padding: 1vw; float: right;">
+            <div style="position: absolute; top: 30px; left: 50px; color: white; font-size: 1.5em;">Drone School</div>
+
+            <div style="position: absolute; top: 20px; left: 270px;">
+                <NuxtImg src="/user_image.png" alt="Imagen" style="width: 75px; height: auto;" />
+            </div>
+
+            <div style="position: absolute; top: 40px; left: 350px; color: white; font-size: 1em;">Admin. 1</div>
+
+            <div style="position: absolute; top: 160px; left: 70px;">
+                <NuxtLink to="/academy/home_academy_admin" style="text-decoration: none;">
+                    <button style="color: black; background-color: white; border: 2px solid black; border-radius: 10px; padding: 5px 10px; margin-right: 10px;">Administrator</button>
+                </NuxtLink>
+                <button style="color: black; background-color: white; border: 2px solid black; border-radius: 10px; padding: 5px 10px;">Instructor</button>
+            </div>
+            <NuxtImg
+                src="/New_Logo-01.jpg"
+                sizes="50vw sm:20vw" 
+            />
+            <div style="color: #4BA9C7; width: 15vw; font-size: 1vw; text-align: right;">DRONE SIMULATOR</div>
+            <NuxtLink><button>Enter Simulation</button></NuxtLink>
+            <div style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); color: black; font-size: 1em; text-align: center;">Please select a role...</div>
+        </div>
+    </NuxtLayout>
+</template>
--- pages/login.vue
+++ pages/login.vue
@@ -1,21 +1,23 @@
 <template>
-    <NuxtLayout>
-        <template #firstSpace>
-            <NuxtLink class="boxHome" to="/"><button>Home Page</button></NuxtLink>
-        </template>
-        <div style="padding: 1vw; float: left;">
-            <NuxtImg
-                src="/New_Logo-01.jpg"
-                sizes="100vw sm:30vw"
-            />
-            <div style="color: #4BA9C7; width: 30vw; font-size: 3vw; text-align: center;">DRONE SIMULATOR</div>
-        </div>
-        <div class="Log">
-            <div class="logForm">E-mail</div>
-            <input class="inputLog" type="email" placeholder="Type here">
-            <div class="logForm">Password</div>
-            <input class="inputLog" type="password" placeholder="Type here"><br>
-            <input type="submit" value="Log in" class="boxLogin">
-        </div>
-    </NuxtLayout>
+  <NuxtLayout>
+    <template #firstSpace>
+      <NuxtLink class="boxHome" to="/"><button>Home Page</button></NuxtLink>
+    </template>
+    <div style="padding: 1vw; float: left;">
+      <NuxtImg
+        src="/New_Logo-01.jpg"
+        sizes="100vw sm:30vw"
+      />
+      <div style="color: #4BA9C7; width: 30vw; font-size: 3vw; text-align: center;">DRONE SIMULATOR</div>
+    </div>
+    <div class="Log">
+      <div class="logForm">E-mail</div>
+      <input class="inputLog" type="email" placeholder="Type here">
+      <div class="logForm">Password</div>
+      <input class="inputLog" type="password" placeholder="Type here"><br>
+      <NuxtLink to="/academy/home_academy">
+        <input type="submit" value="Log in" class="boxLogin">
+      </NuxtLink>
+    </div>
+  </NuxtLayout>
 </template>
