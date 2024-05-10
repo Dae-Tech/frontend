f9f90f03c0487af3e4ba45f7b7e86c694d18de08
jeanpaulsb <jeanpaulsierraboom@gmail.com>
2024-05-10 14:49:53 -0500

    feat: add new hover color

--- components/simulator/sider/Sider.vue
+++ components/simulator/sider/Sider.vue
@@ -1,6 +1,4 @@
 <script setup>
-import { UChip } from "#build/components";
-
 const selected = ref(0);
 const category = ref("");
 
@@ -25,13 +23,6 @@ const options = {
     1: ["Company", "Flight School"],
   },
   Options: { 1: ["Display", "Sound", "Controls", "Language"] },
-  Back: {
-    1: (
-      <UChip>
-        <p>hola</p>
-      </UChip>
-    ),
-  },
 };
 console.log(options["Free Flight"][1]);
 </script>
@@ -48,42 +39,48 @@ console.log(options["Free Flight"][1]);
       class="font-bold text-2xl flex gap-6 flex-col mx-auto mt-60 text-center cursor-pointer"
     >
       <p
-        class="hover:bg-gray-600"
+        class="hover:text-blue-500"
         @click="[setSelected(1), setCategory('Free Flight')]"
       >
         Free Flight
       </p>
       <p
         @click="[setSelected(1), setCategory('Flight Challenges')]"
-        class="hover:bg-gray-600"
+        class="hover:text-blue-500"
       >
         Flight Challenges
       </p>
       <UChip
-        class="hover:bg-gray-600"
+        class="hover:text-blue-500"
         color="yellow"
         @click="[setSelected(1), setCategory('Assignments')]"
         ><p>Assignments</p></UChip
       >
       <p
         @click="[setSelected(1), setCategory('Options')]"
-        class="hover:bg-gray-600"
+        class="hover:text-blue-500"
       >
         Options
       </p>
-      <p class="hover:bg-gray-600">Exit</p>
+      <p class="hover:text-blue-500">Exit</p>
     </div>
   </div>
   <div
     v-if="selected == 1"
-    class="bg-[#2D4C9A] w-[400px] h-screen p-5 flex flex-col text-2xl text-[#f5f5f5] items-center"
+    class="bg-[#2D4C9A] relative w-[400px] h-screen p-5 flex flex-col text-2xl text-[#f5f5f5] items-center"
   >
-    <div class="mt-[265px] flex gap-6 flex-col">
+    <div class="mt-[265px] flex gap-6 flex-col items-center">
       <p
         class="hover:bg-gray-500"
         v-for="option in options[category][selected]"
       >
         {{ option }}
+      </p>
+      <p
+        class="relative top-[500px] cursor-pointer hover:bg-gray-500"
+        @click="setSelected(0)"
+      >
+        Back
       </p>
     </div>
   </div>
