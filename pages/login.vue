2feb25f7482db3d538cc3a6c5d3baad1b540524c
Guzman2808 <guzman.camilo2808@gmail.com>
2024-05-14 02:08:31 -0500

    Add pending pages

--- pages/login.vue
+++ pages/login.vue
@@ -15,7 +15,53 @@
             <input class="inputLog" type="email" placeholder="Type here">
             <div class="logForm">Password</div>
             <input class="inputLog" type="password" placeholder="Type here"><br>
-            <input type="submit" value="Log in" class="boxLogin">
+            <NuxtLink to="/userHome"><input type="submit" value="Log in" class="boxLogin"></NuxtLink>
         </div>
     </NuxtLayout>
 </template>
+
+<style scoped>
+    .boxHome {
+        padding: 5px;
+        margin: 5vh 13px 10px 13px;
+        border: 4px solid white;
+        background-color: #2D4C9A;
+        width: 20vw;
+        float: right;
+        font-size: 25px;
+        text-align: center;
+        border-radius: 1vh;
+        text-decoration: none;
+    }
+    .logForm {
+        padding: 2vh 3vw 2vh 3vw;
+        margin: 0vh 2vw 0vh 2vw;
+        font-size: 3vh;
+        width: 35vw;
+        text-align: center;
+    }
+    .inputLog {
+        width: 30vw;
+        margin-left: 5vw;
+        font-size: 2.5vh;
+        border-radius: 2vh;
+        padding-left: 0.5vw;
+        height: 5vh;
+        background-color: white;
+    }
+    .Log{
+        float: right;
+        margin-top: 5vh;
+        margin-right: 3vw;
+        background-color: #2D4C9A;
+        height: 60vh;
+        border-radius: 2vh;
+    }
+    .Log .boxLogin{
+        float: none;
+        margin: 20vh 4.5vw 2vh 4.5vw;
+        padding: 0vh 2vw 0vh 2vw;
+        width: 30vw;
+        height: 7vh;
+    }
+</style>
---
+++ pages/paymentLoading.vue
@@ -0,0 +1,48 @@
+<template>
+  <NuxtLayout>
+    <template #firstSpace>
+      <div class="tittleFirstSpace">Redirecting to payment services</div>
+      <NuxtLink class="boxHome" to="/"><button>Home Page</button></NuxtLink>
+    </template>
+    <div>
+      <NuxtImg
+        format="gif"
+        src="/Loading.gif"
+        style="padding: 3.5vh 3vw 0vh 3vw; width: 25vw; align-content: center; margin-left: 37.5vw;"
+      />
+      <div class="loading">Please do not exit or refresh the page.</div>
+    </div>
+  </NuxtLayout>
+</template>
+
+<style  scoped>
+  .boxHome {
+    padding: 5px;
+    margin: 5vh 13px 10px 13px;
+    border: 4px solid white;
+    background-color: #2D4C9A;
+    width: 20vw;
+    float: right;
+    font-size: 25px;
+    text-align: center;
+    border-radius: 1vh;
+    text-decoration: none;
+  }
+  .tittleFirstSpace {
+    padding: 0px;
+    margin: 5vh 13px 10px 13px;
+    box-sizing: border-box;
+    font-size: 5.5vh;
+    width: 75vw;
+    float: left;
+    text-align: center;
+  }
+  .loading{
+    padding-left: 30vw;
+    padding-right: 30vw;
+    padding-top: 4vh;
+    font-size: 4vh;
+    text-align: center;
+    color: black;
+  }
+</style>
