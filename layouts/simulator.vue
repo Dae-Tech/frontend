7513aeb60c587e90589f3da52605a20c3155d58e
jeanpaulsb <jeanpaulsierraboom@gmail.com>
2024-06-28 00:59:27 -0500

    add missing iframe

--- layouts/simulator.vue
+++ layouts/simulator.vue
@@ -1,6 +1,8 @@
 <template>
   <main class="flex h-screen bg-red-40 w-screen">
     <SimulatorSider></SimulatorSider>
-    <slot></slot>
+    <slot>
+      <iframe src="http://40.113.49.19:8080/"></iframe>
+    </slot>
   </main>
 </template>
