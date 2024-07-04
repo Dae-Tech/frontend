17731711e59fb123aad236977fe7f2ac6295dd7d
Jean Paul <88939729+JeanPaulSB@users.noreply.github.com>
2024-07-03 20:02:29 -0500

    Update simulator.vue

--- layouts/simulator.vue
+++ layouts/simulator.vue
@@ -2,7 +2,7 @@
   <main class="flex h-screen bg-red-40 w-screen">
     <SimulatorSider></SimulatorSider>
     <slot>
-      <iframe class="w-full" src="https://api.memorise.com.co"></iframe>
+      <iframe class="w-full" src="https://api.memorise.com.co:8080"></iframe>
     </slot>
   </main>
 </template>
