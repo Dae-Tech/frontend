4e7aeebbb4016e3e7d4238259b9c1eb328951c04
jeanpaulsb <jeanpaulsierraboom@gmail.com>
2024-07-17 20:33:14 -0500

    add new styles

--- components/simulator/joystick/Joystick.vue
+++ components/simulator/joystick/Joystick.vue
@@ -1,14 +1,12 @@
 <script setup lang="ts">
-import { computed } from "vue";
-const throttle = ref(0);
-const roll = ref(0);
-const pitch = ref(0);
-const yaw = ref(0);
-
-const getFirstAxis = computed(() => {
-  const base = 123;
-  return "top-[" + (base + throttle.value * 10).toString() + "px]";
+import { reactive } from "vue";
+const controls = reactive({
+  throttle: 0,
+  roll: 0,
+  pitch: 0,
 });
+
+const isConnected = ref(false);
 
 interface ControlsInput {
   roll: number;
@@ -18,18 +16,17 @@ interface ControlsInput {
 
 function submitInputs() {
   const controlsInput: ControlsInput = {
-    throttle: throttle.value,
-    roll: roll.value,
-    pitch: pitch.value,
+    throttle: controls.throttle,
+    roll: controls.roll,
+    pitch: controls.pitch,
   };
-
-  console.log("enviamos dato");
   connection.send(JSON.stringify(controlsInput));
 }
+
 const connection = new WebSocket("ws://20.123.77.40:3000");
 
 connection.onopen = function (event) {
-  setInterval(() => submitInputs(), 400);
+  setInterval(() => submitInputs(), 100);
 };
 
 const map = new Map();
@@ -66,34 +63,35 @@ const keyPressed = ref("");
 
 window.addEventListener("keydown", (e) => {
   keyPressed.value = e.key;
-  if (e.key == "ArrowUp") {
-    if (validCommand(throttle.value, 0.1, 1, "<=")) {
-      throttle.value += 0.1;
+  console.log(keyPressed.value);
+  if (e.key == "W" || e.key == "w") {
+    if (validCommand(controls.throttle, 0.1, 1, "<=")) {
+      controls.throttle += 0.1;
     }
   }
-  if (e.key == "ArrowDown") {
-    if (validCommand(throttle.value, -0.1, 0, ">")) {
-      throttle.value -= 0.1;
+  if (e.key == "s" || e.key == "S") {
+    if (validCommand(controls.throttle, -0.1, 0, ">")) {
+      controls.throttle -= 0.1;
     }
   }
   if (e.key == "ArrowRight") {
-    if (validCommand(roll.value, 0.1, 1, "<=")) {
-      roll.value += 0.1;
+    if (validCommand(controls.roll, 0.1, 1, "<=")) {
+      controls.roll += 0.1;
     }
   }
   if (e.key == "ArrowLeft") {
-    if (validCommand(roll.value, -0.1, -1, ">=")) {
-      roll.value -= 0.1;
+    if (validCommand(controls.roll, -0.1, -1, ">=")) {
+      controls.roll -= 0.1;
     }
   }
-  if (e.key == "8") {
-    if (validCommand(pitch.value, 0.1, 1, "<=")) {
-      pitch.value += 0.1;
+  if (e.key == "ArrowUp") {
+    if (validCommand(controls.pitch, 0.1, 1, "<=")) {
+      controls.pitch += 0.1;
     }
   }
-  if (e.key == "2") {
-    if (validCommand(pitch.value, -0.1, -1, ">=")) {
-      pitch.value -= 0.1;
+  if (e.key == "ArrowDown") {
+    if (validCommand(controls.pitch, -0.1, -1, ">=")) {
+      controls.pitch -= 0.1;
     }
   }
 });
@@ -102,14 +100,21 @@ window.addEventListener("keydown", (e) => {
   <div
     class="h-screen w-[400px] bg-white flex items-center gap-8 p-2 flex-col justify-center"
   >
-    <div class="bg-gray-200 p-4 shadow-lg rounded-lg">
+    <UBadge
+      size="lg"
+      variant="outline"
+      :color="isConnected ? 'green' : 'red'"
+      :label="isConnected ? 'Conectado' : 'Desconectado'"
+    ></UBadge>
+    <div class="bg-gray-200 p-4 shadow-lg rounded-lg flex flex-col gap-2">
       <p class="text-black text-center">Controles</p>
+
       <div class="text-black flex gap-2">
-        <UKbd>↑</UKbd>
+        <UKbd>W</UKbd>
         <p>: Aumentar throttle</p>
       </div>
       <div class="text-black flex gap-2">
-        <UKbd>↓</UKbd>
+        <UKbd>S</UKbd>
         <p>: Disminuir throttle</p>
       </div>
       <div class="text-black flex gap-2">
@@ -121,35 +126,38 @@ window.addEventListener("keydown", (e) => {
         <p>: Roll hacia la derecha</p>
       </div>
       <div class="text-black flex gap-2">
-        <UKbd>8</UKbd>
+        <UKbd>↑</UKbd>
         <p>: Pitch hacia arriba</p>
       </div>
       <div class="text-black flex gap-2">
-        <UKbd>2</UKbd>
+        <UKbd>↓</UKbd>
         <p>: Pitch hacia abajo</p>
       </div>
     </div>
     <div
       class="flex flex-col justify-center items-center bg-gray-200 p-4 shadow-xl rounded-lg"
     >
-      <UButton :icon="map.get(keyPressed)"></UButton>
+      <UButton :icon="map.get(keyPressed)">{{ keyPressed }}</UButton>
       <div>
         <p class="text-black text-center">Throttle</p>
-        <UProgress :value="throttle * 100" indicator></UProgress>
+        <UProgress :value="controls.throttle * 100" indicator></UProgress>
       </div>
-      <p class="text-black">Roll: {{ roll.toFixed(3) }}</p>
-      <p class="text-black">Pitch: {{ pitch.toFixed(3) }}</p>
+      <p class="text-black">Roll: {{ controls.roll.toFixed(3) }}</p>
+      <p class="text-black">Pitch: {{ controls.pitch.toFixed(3) }}</p>
     </div>
     <div
       class="w-[300px] h-[300px] bg-center bg-no-repeat bg-contain"
       :class="['bg-[url(/joystick.png)]']"
     >
       <div
-        :style="{ top: `${125 - throttle * 10}px` }"
+        :style="{ top: `${125 - controls.throttle * 10}px` }"
         class="w-[50px] h-[50px] bg-blue-500 rounded-full relative left-[78px]"
       ></div>
       <div
-        :style="{ top: `${76 - pitch * 10}px`, left: `${174 + roll * 10}px` }"
+        :style="{
+          top: `${76 - controls.pitch * 10}px`,
+          left: `${174 + controls.roll * 10}px`,
+        }"
         class="w-[50px] h-[50px] bg-red-500 rounded-full relative"
       ></div>
     </div>
