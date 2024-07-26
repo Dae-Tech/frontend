293e7553baa6d82c1c162fd12947bcc00c6d58e9
jangel017 <142262468+jangel017@users.noreply.github.com>
2024-07-26 00:40:25 -0500

    solving template

--- components/simulator/joystick/Joystick.vue
+++ components/simulator/joystick/Joystick.vue
@@ -109,76 +109,18 @@ onUnmounted(() => {
 });
 </script>
 <template>
-  <div
-    class="h-screen w-[400px] bg-white flex items-center gap-8 p-2 flex-col justify-center"
-  >
-    <!-- Estado de conexión -->
-    <UBadge
-      size="lg"
-      variant="outline"
-      :color="isConnected ? 'green' : 'red'"
-      :label="isConnected ? 'Conectado' : 'Desconectado'"
-      class="mb-4"
-    ></UBadge>
-
-    <!-- Información sobre los controles -->
+  <div class="h-screen w-[400px] bg-white flex items-center gap-8 p-2 flex-col justify-center">
+    <UBadge size="lg" variant="outline" :color="isConnected ? 'green' : 'red'" :label="isConnected ? 'Conectado' : 'Desconectado'"></UBadge>
     <div class="bg-gray-200 p-4 shadow-lg rounded-lg flex flex-col gap-2">
       <p class="text-black text-center font-bold">Controles</p>
-      
-      <div class="text-black flex gap-2">
-        <UKbd>W</UKbd>
-        <p>: Aumentar throttle</p>
-      </div>
-      <div class="text-black flex gap-2">
-        <UKbd>S</UKbd>
-        <p>: Disminuir throttle</p>
-      </div>
-      <div class="text-black flex gap-2">
-        <UKbd>←</UKbd>
-        <p>: Roll hacia la izquierda</p>
-      </div>
-      <div class="text-black flex gap-2">
-        <UKbd>→</UKbd>
-        <p>: Roll hacia la derecha</p>
-      </div>
-      <div class="text-black flex gap-2">
-        <UKbd>↑</UKbd>
-        <p>: Pitch hacia arriba</p>
-      </div>
-      <div class="text-black flex gap-2">
-        <UKbd>↓</UKbd>
-        <p>: Pitch hacia abajo</p>
-      </div>
     </div>
-
-    <!-- Muestra del botón del joystick -->
-    <UButton :icon="map.get(keyPressed)">{{ keyPressed }}</UButton>
-
-    <!-- Información de los controles -->
     <p class="text-black text-center">Throttle</p>
     <UProgress :value="controls.throttle * 100" indicator></UProgress>
-
     <p class="text-black">Roll: {{ controls.roll.toFixed(3) }}</p>
     <p class="text-black">Pitch: {{ controls.pitch.toFixed(3) }}</p>
-
-    <!-- Visualización gráfica del joystick -->
-    <div
-      class="w-[300px] h-[300px] bg-center bg-no-repeat bg-contain"
-      :class="['bg-[url(/joystick.png)]']"
-    >
-      <!-- Indicador de throttle -->
-      <div
-        :style="{ top: `${125 - controls.throttle * 10}px` }"
-        class="w-[50px] h-[50px] bg-blue-500 rounded-full relative left-[78px]"
-      ></div>
-      <!-- Indicador de roll y pitch -->
-      <div
-        :style="{
-          top: `${76 - controls.pitch * 10}px`,
-          left: `${174 + controls.roll * 10}px`,
-        }"
-        class="w-[50px] h-[50px] bg-red-500 rounded-full relative"
-      ></div>
+    <div class="w-[300px] h-[300px] bg-center bg-no-repeat bg-contain" :class="['bg-[url(/joystick.png)]']">
+      <div :style="{ top: `${125 - controls.throttle * 10}px` }" class="w-[50px] h-[50px] bg-blue-500 rounded-full relative left-[78px]"></div>
+      <div :style="{ top: `${76 - controls.pitch * 10}px`, left: `${174 + controls.roll * 10}px` }" class="w-[50px] h-[50px] bg-red-500 rounded-full relative"></div>
     </div>
   </div>
 </template>
